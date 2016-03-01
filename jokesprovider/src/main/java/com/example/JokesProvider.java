30959533d345aa8a4f46d168f3589e9e11fc1b27
dmytroKarataiev <karataev.dima@gmail.com>
2016-02-29 20:14:25 -0800

    add: java library

---
+++ jokesprovider/src/main/java/com/example/JokesProvider.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import java.util.Random;
+
+public class JokesProvider {
+
+    Random random;
+
+    String[] jokes = {
+            "First Joke",
+            "Second Joke",
+            "Third Joke",
+            "I'm not a funny guy",
+            "Lame joke",
+            "Not a joke"};
+
+    public JokesProvider() {
+        random = new Random();
+    }
+
+    public String getJoke() {
+        int jokeNumber = random.nextInt(jokes.length);
+        return jokes[jokeNumber];
+    }
+}
