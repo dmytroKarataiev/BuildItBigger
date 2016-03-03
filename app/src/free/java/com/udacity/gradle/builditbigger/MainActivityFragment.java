2cb84be733a6442847bdcd5fddf0e4469fe44555
dmytroKarataiev <karataev.dima@gmail.com>
2016-03-03 15:55:35 -0800

    add: interstitial add

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -8,8 +8,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
+import com.google.android.gms.ads.doubleclick.PublisherInterstitialAd;
 
 
 /**
@@ -18,6 +21,9 @@ import com.google.android.gms.ads.AdView;
 public class MainActivityFragment extends Fragment {
 
     private final static String LOG_TAG = MainActivityFragment.class.getSimpleName();
+
+    // add interstitial add
+    PublisherInterstitialAd mPublisherInterstitialAd;
 
     public MainActivityFragment() {
     }
@@ -32,7 +38,8 @@ public class MainActivityFragment extends Fragment {
         // get test ads on a physical device. e.g.
         // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
         AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                //.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("611F2A146CD0E2D8437BE2B61D07C6D8")
                 .build();
         mAdView.loadAd(adRequest);
 
@@ -40,9 +47,26 @@ public class MainActivityFragment extends Fragment {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (mPublisherInterstitialAd.isLoaded()) {
+                    mPublisherInterstitialAd.show();
+                } else {
+                    tellJoke(getActivity());
+                }
+            }
+        });
+
+        mPublisherInterstitialAd = new PublisherInterstitialAd(getActivity());
+        mPublisherInterstitialAd.setAdUnitId("/6499/example/interstitial");
+
+        mPublisherInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
                 tellJoke(getActivity());
             }
         });
+
+        requestNewInterstitial();
 
         return root;
     }
@@ -50,6 +74,14 @@ public class MainActivityFragment extends Fragment {
     public void tellJoke(Activity activity) {
         FetchJokesEndpoint endpointsAsyncTask = new FetchJokesEndpoint();
         endpointsAsyncTask.execute(activity);
+    }
+
+    private void requestNewInterstitial() {
+        PublisherAdRequest adRequest = new PublisherAdRequest.Builder()
+                .addTestDevice("611F2A146CD0E2D8437BE2B61D07C6D8")
+                .build();
+
+        mPublisherInterstitialAd.loadAd(adRequest);
     }
 
 }
